a63541979dcdeed20fc4c0fd254c9b913709adcb
Bertan Otun <bertanotun@gmail.com>
2025-01-13 10:35:04 +0100

    info en about gemaakt

---
+++ about.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Focus - Over Ons</title>
+    <link rel="stylesheet" href="style/about.css">
+</head>
+<body>
+<header>
+    <?php include('Includes/header.php ') ?>
+</header>
+    <section class="map-section">
+        <div class="text-content">
+            <h2>Over Onze Locatie</h2>
+            <p>Onze sportschool biedt moderne faciliteiten en een gastvrije omgeving.</p>
+        </div>
+        <iframe
+            id="map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.0654266162887!2d4.2162758!3d51.6952478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c4440bd4e718b3%3A0xc1cda3e0e689459a!2sFOCUS%20Health%20%26%20Fitness!5e0!3m2!1snl!2snl!4v1736725332090!5m2!1snl!2snl"
+            allowfullscreen
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
+        <div class="text-content">
+            <h2>Contact Informatie</h2>
+            <p>Adres: Middelharnis, Oude-Tonge en Ouddorp.</p>
+        </div>
+    </section>
+</body>
+ </main>
+    <footer>
+        <?php include('Includes/footer.php ') ?>
+    </footer>
+</body>
+</html>
+
