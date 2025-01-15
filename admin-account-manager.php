aeff9b1573b468192aa49bff24ad1172ea46203b
Bertan Otun <bertanotun@gmail.com>
2025-01-15 14:14:55 +0100

    admin account dashboard en css ervan

--- admin-account-manager.php
+++ admin-account-manager.php
@@ -1,1 +1,35 @@
 <?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/admin-account-dashboard.css">
+</head>
+<body>
+<?php include('Includes/header.php') ?>
+
+<section class="search-bar">
+    <form action="search.php" method="get">
+        <input type="text" name="query" placeholder="Zoeken..." required>
+        <button type="submit">Zoeken</button>
+    </form>
+</section>
+
+<section class="dashboard-section">
+    <div class="text-content">
+        <h2></h2>
+        <p></p>
+    </div>
+    <div class="text-content">
+        <h2>Contact Informatie</h2>
+        <p>Adres: Middelharnis, Oude-Tonge en Ouddorp.</p>
+    </div>
+</section>
+
+<?php include('Includes/footer.php') ?>
+</body>
+</html>
