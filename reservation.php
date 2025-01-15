2297c13c88eedf80767bab995cbac509c211b5e5
olivi <1101595@hr.nl>
2025-01-15 14:19:44 +0100

    mail hertoegevoegd de

--- reservation.php
+++ reservation.php
@@ -90,6 +90,13 @@ if (isset($_POST['submit'])) {
         }
 
         mysqli_close($db);
+
+        //De confirmation mail verzenden
+        $to = $email;
+        $subject = 'bevestiging padelbaan reservering';
+        $fullMessage = 'Beste' . $firstName . $lastName . "\n" . 'Bedankt voor uw reservering:' . "\n" . date('d F Y', strtotime($date)) . "\n" . date('H:i', strtotime($date)) . "\n" . 'Baan' . $courseID;
+        mail($to, $subject, $fullMessage);
+
         header('Location: confirmation.php');
     }
 }
@@ -108,31 +115,41 @@ if (isset($_POST['submit'])) {
     <div class="reservation-overzicht"> U reserveert voor <?= date('d F Y', strtotime($date)) ?> om <?= date('H:i', strtotime($date)) ?> voor baan <?= $courseID?></div>
     <section class="reservation-form">
         <form action="" method="post">
-            <div class="formInput">
-                <label for="firstName">Voornaam</label>
-                <input class="input" id="firstName" type="text" maxlength="30" name="firstName"
-                       value="<?= htmlspecialchars($user['first_name'] ?? '') ?>"/>
-            </div>
-            <p><?= $errors['firstName'] ?? '' ?></p>
-            <div class="formInput">
-                <label for="lastName">Achternaam</label>
-                <input class="input" id="lastName" type="text" maxlength="30" name="lastName"
-                       value="<?= htmlspecialchars($user['last_name'] ?? '') ?>"/>
-            </div>
-            <p><?= $errors['lastName'] ?? '' ?></p>
-            <div class="formInput">
-                <label for="email">Email-adres</label>
-                <input class="input" id="email" type="email" maxlength="30" name="email"
-                       value="<?= htmlspecialchars($user['email'] ?? '') ?>"/>
-            </div>
-            <p><?= $errors['email'] ?? '' ?></p>
-            <div class="formInput">
-                <label for="phoneNumber">Telefoonnummer</label>
-                <input class="input" id="phoneNumber" type="tel" maxlength="10" name="phoneNumber"
-                       value="<?= htmlspecialchars($user['phone_number'] ?? '') ?>"/>
-            </div>
-            <p><?= $errors['phoneNumber'] ?? '' ?></p>
-            <button class="submitButton" type="submit" name="submit">Reserveer</button>
+            <?php if (!isset($_SESSION['user_id'])) { ?>
+                <div class="formInput">
+                    <label for="firstName">Voornaam</label>
+                    <input class="input" id="firstName" type="text" maxlength="30" name="firstName"
+                           value="<?= htmlspecialchars($user['first_name'] ?? '') ?>"/>
+                </div>
+                <p><?= $errors['firstName'] ?? '' ?></p>
+                <div class="formInput">
+                    <label for="lastName">Achternaam</label>
+                    <input class="input" id="lastName" type="text" maxlength="30" name="lastName"
+                           value="<?= htmlspecialchars($user['last_name'] ?? '') ?>"/>
+                </div>
+                <p><?= $errors['lastName'] ?? '' ?></p>
+                <div class="formInput">
+                    <label for="email">Email-adres</label>
+                    <input class="input" id="email" type="email" maxlength="30" name="email"
+                           value="<?= htmlspecialchars($user['email'] ?? '') ?>"/>
+                </div>
+                <p><?= $errors['email'] ?? '' ?></p>
+            <?php } else { ?>
+                <input type="hidden" id="firstName" name="firstName" value="<?= $_SESSION['first_name'] ?>">
+                <input type="hidden" id="lastName" name="lastName" value="<?= $_SESSION['last_name'] ?>">
+                <input type="hidden" id="email" name="email" value="<?= $_SESSION['email'] ?>">
+            <?php } ?>
+            <?php if (!isset($_SESSION['phone_number'])) { ?>
+                <div class="formInput">
+                    <label for="phoneNumber">Telefoonnummer</label>
+                    <input class="input" id="phoneNumber" type="tel" maxlength="10" name="phoneNumber"
+                           value="<?= htmlspecialchars($user['phone_number'] ?? '') ?>"/>
+                </div>
+                <p><?= $errors['phoneNumber'] ?? '' ?></p>
+                <button class="submitButton" type="submit" name="submit">Reserveer</button>
+            <?php } else { ?>
+                <input type="hidden" id="phoneNumber" name="phoneNumber" value="<?= $_SESSION['phone_number'] ?>">
+            <?php } ?>
         </form>
     </section>
 </main>
