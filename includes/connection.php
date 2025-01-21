f3f2d653b9c58119aeed3f31d3a5be6ae304380f
HiddeBouwman <bouwmanhidde@gmail.com>
2025-01-21 15:20:41 +0100

    connection.php gerepareerd

--- includes/connection.php
+++ includes/connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql.hosted.hro.nl";
-$database = "prj_2024_2025_cle2_t10";
-$user = "prj_2024_2025_cle2_t10";
-$password = "hausoong";
+$host = "127.0.0.1";
+$database = "padel_systeem";
+$user = "root";
+$password = "";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
