08ad0f4ee1fdab1d1018ab7bd14930cf8e81ae27
olivi <1101595@hr.nl>
2025-01-21 14:42:07 +0100

    a

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ if (isset($_COOKIE['update_message'])) {
         </div>
     </section>
 <?php }
-if (isset($_SESSION['is_admin']) != 1) { ?> ?>
+if (!isset($_SESSION['is_admin'])) { ?> ?>
     <section class="main-section">
         <a href="info.php">Meer over padel</a>
         <a href="about.php">Meer over Focus</a>
